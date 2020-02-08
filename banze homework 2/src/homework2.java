ae6197a391b198a3241f8d0519602bcc4bb30ecf
kevinbanze <kevinbanze@localhost>
2020-02-08 01:12:56 -0500

    ready for review

---
+++ banze homework 2/src/homework2.java
@@ -0,0 +1,25 @@
+// Kevin Banze
+// Homwework 2
+// 2 - 6 - 2020
+public class homework2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// declaring variables
+		int numberOfDrinks;
+		int poundsOfMeat;
+		byte age;
+		String x = "Kevin";
+		age = 24;
+		poundsOfMeat = 1;
+		numberOfDrinks = 3;
+		
+		
+		//outputting variables 
+		System.out.print(poundsOfMeat + numberOfDrinks + "\n");
+		System.out.println("I am " + x + " I am " + age + ".");
+		System.out.println("I need to get " + poundsOfMeat + " oz of meat .");	
+		
+	}
+
+}
